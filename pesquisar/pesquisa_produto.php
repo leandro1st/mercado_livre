073d57312155d519b29d272243314465df7916b2
leandro1st <leandrostsakamoto@gmail.com>
2020-07-16 21:02:39 -0300

    remoção de espaços em branco da string, db

--- pesquisar/pesquisa_produto.php
+++ pesquisar/pesquisa_produto.php
@@ -3,7 +3,7 @@
 require('../externo/connect.php');
 
 $produto_atual = $_POST['atual'];
-$cod_athos_produto = $_POST['cod_athos_' . $produto_atual];
+$cod_athos_produto = trim($_POST['cod_athos_' . $produto_atual]);
 
 $pesquisa_produto = mysqli_query($connect, "SELECT * FROM $produtos WHERE $cod_athos = '$cod_athos_produto'");
 $vetor_produto = mysqli_fetch_array($pesquisa_produto);
